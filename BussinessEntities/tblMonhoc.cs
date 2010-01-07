af1530e0cedd7e98caee1353d73e19c7bc344c1c
hungnt.it <hungnt.it@5291d9ee-fb86-11de-930f-d36749347e7b>
2010-01-07 12:22:56 +0000

    add project

---
+++ BussinessEntities/tblMonhoc.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BussinessEntities
+{
+    class tblMonhoc
+    {
+        #region FIELD
+        private String _maMon;
+        private String _tenMon;
+        private int _tinchiLyThuyet;
+        private int _tinchiThucHanh;
+        #endregion
+        #region Constructors
+        public tblMonhoc(String maMon, String tenMon, int soTinchiLythuyet, int soTinchiThucHanh)
+        {
+            this._maMon = maMon;
+            this._tenMon = tenMon;
+            this._tinchiLyThuyet = soTinchiLythuyet;
+            this._tinchiThucHanh = soTinchiThucHanh;
+        }
+        #endregion
+
+        #region Properties
+        public String MaMon
+        {
+            get { return _maMon; }
+            set { _maMon = value; }
+        }
+
+        public String TenMon
+        {
+            get { return _tenMon; }
+            set { _tenMon = value; }
+        }
+       
+
+        public int TinchiLyThuyet
+        {
+            get { return _tinchiLyThuyet; }
+            set { _tinchiLyThuyet = value; }
+        }
+       
+
+        public int TinchiThucHanh
+        {
+            get { return _tinchiThucHanh; }
+            set { _tinchiThucHanh = value; }
+        }
+        #endregion
+    }
+}
---
+++ BussinessEntities/tblSinhVien.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BussinessEntities
+{
+    class tblSinhVien:tblPerson
+    {
+        #region Field
+        private String _maSV;
+        private String _maKhoa;
+        private int _nienkhoa;
+      
+
+        #endregion
+        #region Constructor
+        
+        public tblSinhVien(String maSV, String maKhoa, String hoSV, String tenSV, String cmnd, DateTime ngaysinh, Boolean gioitinh, String noisinh, String diachi, int nienkhoa):base(hoSV,tenSV,cmnd,ngaysinh,gioitinh,noisinh,diachi)
+        {
+            this._maSV          = maSV;
+            this._maKhoa        = maKhoa;
+            this._nienkhoa      = nienkhoa;
+        }
+
+
+        #endregion
+        #region Properties
+
+        public String MaSV
+        {
+            get
+            {
+                return _maSV;
+
+            }
+            set
+            {
+                _maSV = value;
+            }
+
+        }
+        public String MaKhoa
+        {
+            get
+            {
+                return _maKhoa;
+
+            }
+            set
+            {
+                _maKhoa = value;
+            }
+
+        }
+       
+        
+
+        public int NienKhoa
+        {
+            get { return _nienkhoa; }
+            set { _nienkhoa = value; }
+        }
+        #endregion
+    }
+
+}
